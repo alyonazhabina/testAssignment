6d13b02c258593ba2371da122d77c12b9abb3698
Alena Zhabina <zaa145@yandex.ru>
2020-07-17 19:22:10 +0300

    Добавлен функционал:  - при нахождении аттрибута тега начинающегося с “@” (собачки) на него навешивается соответствующий слушатель события, исходный аттрибут удаляется  - при нахождении аттрибута тега начинающегося с “:” (двоеточия) он заменяется на аттрибут без “:”, но с окончательным расчетным значением
     Префиксы @ и : вынесены в отдельные переменные

--- Component.js
+++ Component.js
@@ -1,13 +1,17 @@
 export class Component {
 	static assoc = {};
+	static EVENT_PREFIX = '@';
+	static REACTIVE_PROP_PREFIX = ':';
 
 	constructor(props = {}) {
+		console.log('constructor function______________' + this.constructor);
 		this.template_content = undefined;
 		this.template_file = undefined;
 		Object.assign((this.props = {}), props);
 	}
 
 	get dom() {
+		console.log('dom function______________');
 		return new Promise((rv) => {
 			this.template.then((template) => {
 				console.log({template});
@@ -17,6 +21,7 @@ export class Component {
 	}
 
 	get template() {
+		console.log('template function______________');
 		return new Promise((rv) => {
 			if (this.template_content) {
 				rv(this.template_content);
@@ -28,18 +33,34 @@ export class Component {
 			fetch(this.template_file).then((resp) => {
 				resp.text().then((text) => {
 					this.template_content = text;
+					console.log('type');
+					console.log(typeof text)
 					rv(this.template_content);
 				});
 			});
 		});
 	}
 
+	/**
+	 *
+	 * @param name
+	 * @returns {Promise<*>}
+	 * @private
+	 */
 	async _get(name) {
+		console.log('_get function______________');
 		if (typeof this.props[name] !== "undefined") return this.props[name];
 		return this[name];
 	}
 
+	/**
+	 *
+	 * @param name
+	 * @returns {Promise<*>}
+	 * @private
+	 */
 	async _get_value(name) {
+		console.log('_get_value function______________');
 		let r = await this._get(name);
 		if (r === undefined) {
 			try {
@@ -54,7 +75,13 @@ export class Component {
 		return r;
 	}
 
+	/**
+	 *
+	 * @param s
+	 * @returns {Promise<*>}
+	 */
 	async renderString(s) {
+		console.log('renderString function______________');
 		let off = 0;
 
 
@@ -66,19 +93,28 @@ export class Component {
 		return s;
 	}
 
-	async parseAttributes(attributes) {
+	async parseAttributes(node) {
+		console.log('parseAttributes function______________');
+		const attributes = node.attributes;
 		const o = {};
 		await Promise.all(
 			Array.from(attributes).map(async (a) => {
+				console.log('attributes = ');
+				console.log(a)
 				let name1 = a.name.slice(1);
 				switch (a.name[0]) {
-					case "@":
+					case this.constructor.EVENT_PREFIX:
 						// event
 						(o._events || (o._events = {}))[name1] = await this._get(a.value);
+						await this.addEventHandler(node, name1,o._events[name1])
 						break;
-					case ":":
+					case this.constructor.REACTIVE_PROP_PREFIX:
 						// reactive prop
+
 						o[name1] = await this._get_value(a.value);
+						console.log(this.constructor)
+						console.log(name1+" "+o[name1])
+						await this.addAttribute(node, name1, o[name1], this.constructor.REACTIVE_PROP_PREFIX)
 						break;
 					default:
 						// prop
@@ -89,13 +125,28 @@ export class Component {
 		return o;
 	}
 
+	/**
+	 *
+	 * @param parent
+	 * @returns {Promise<void>}
+	 */
 	async renderChilds(parent) {
+		console.log('renderChilds function______________');
+		console.log('PARENT:');
+		console.log(parent);
+		console.log('this.props');
+		console.log(this.props);
+		console.log(this.constructor)
 		for (let node of parent.childNodes) {
+			console.log('node before')
+			console.log(node)
 			switch (node.nodeType) {
 				case 1: // tag
 					const tag = node.tagName.toLowerCase(),
 						tag_class = this.constructor.assoc[tag],
-						props = await this.parseAttributes(node.attributes);
+						props = await this.parseAttributes(node);
+					console.log('tag: ' + tag);
+					console.log(node);
 
 					console.log({tag, props});
 
@@ -115,8 +166,38 @@ export class Component {
 		}
 	}
 
+	async addAttribute(node, attrName, attrValue, prefix){
+		node.setAttribute(attrName, attrValue);
+		await this.removeRelatedAttribute(node, attrName, prefix)
+	}
+
+
+	async addEventHandler(node, eventName, eventHandler) {
+		console.log(event);
+		console.log('addEventListener function______________');
+		// for (let event in events) {
+			node.addEventListener(eventName, eventHandler);
+			await this.removeRelatedAttribute(node, eventName, this.constructor.EVENT_PREFIX)
+		// }
+	}
+
+	/**
+	 * Удаляет аттрибут узла по имени аттрибута и префиксу
+	 * @param node узел
+	 * @param attrName имя аттрибута
+	 * @param prefix префикс
+	 * @returns {Promise<void>}
+	 */
+	async removeRelatedAttribute(node, attrName, prefix) {
+		if (node && attrName) {
+			node.removeAttribute((prefix || '') + attrName);
+			console.log(`Attribute ${attrName} has been removed`);
+		}
+	}
+
 	async renderWithBody(element, body) {
 		await this.renderChilds(body);
+		console.log(body.childNodes)
 		element.replaceWith(...Array.from(body.childNodes));
 	}
 
